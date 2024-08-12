3d10af41f5973bbbc356f162315a8e699c646e5d
Cristian Sena <csena@gett.mobi>
2024-08-12 18:12:49 -0300

    agrego columna de consumo

---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import type { MenuItem, OrderItem } from '../types';
+
+export default function useOrder() {
+
+    const [order, setOrder] = useState<OrderItem[]>([]);
+
+    const addItem = (item: MenuItem) => {
+
+        const existingItem = order.find((orderItem) => orderItem.id === item.id);
+
+        if (existingItem) {
+            const updatedOrder = order.map((orderItem) => {
+                if (orderItem.id === item.id) {
+                    return {
+                        ...orderItem,
+                        quantity: orderItem.quantity + 1
+                    }
+                } else {
+                    return orderItem
+                }
+            })
+            setOrder(updatedOrder)
+            return
+        }
+        
+        const newItem: OrderItem = {
+            ...item,
+            quantity: 1
+        }
+        setOrder([...order, newItem])
+    }
+
+
+    const removeItem = (id: MenuItem['id']) => {
+
+        const existingItem = order.find((orderItem) => orderItem.id === id);
+
+        if (existingItem && existingItem.quantity > 1) {
+            const updatedOrder = order.map((orderItem) => {
+                if (orderItem.id === id) {
+                    return {
+                        ...orderItem,
+                        quantity: orderItem.quantity - 1
+                    }
+                } else {
+                    return orderItem
+                }
+            })
+            setOrder(updatedOrder)
+            return
+        }
+
+        setOrder(order.filter((item) => item.id !== id));
+    }
+
+    return {
+        order,
+        addItem,
+        removeItem
+    }
+}
