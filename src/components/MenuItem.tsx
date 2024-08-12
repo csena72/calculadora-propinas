2327b232e66d364da0ca7ad08854bdc1a7bb9929
Cristian Sena <csena@gett.mobi>
2024-08-12 12:13:06 -0300

    Agrego menuItems

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,21 @@
+import type { MenuItem } from '../types';
+
+type MenuItemProps = {
+  item: MenuItem
+}
+
+const MenuItem = ( { item } : MenuItemProps ) => {
+
+
+  return (
+    <button
+      className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+      onClick={() => console.log(item)}
+    >
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
+  )
+}
+
+export default MenuItem
